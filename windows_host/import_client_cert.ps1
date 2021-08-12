726d8d756c78981beecaa5976ad3f11c85f8caee
sandipsable111 <40589999+sandipsable111@users.noreply.github.com>
2021-08-12 21:36:09 +0530

    Update import_client_cert.ps1
    cert path updated

--- windows_host/import_client_cert.ps1
+++ windows_host/import_client_cert.ps1
@@ -1,5 +1,5 @@
 # path to the genereated client cert
-$pubKeyFilePath = '..\ansible_server\cert.pem'
+$pubKeyFilePath = '..\Ansible\ansible_cert.pem'
 
 # Import the public key into Trusted Root Certification Authorities and Trusted People
 $null = Import-Certificate -FilePath $pubKeyFilePath -CertStoreLocation 'Cert:\LocalMachine\Root'
